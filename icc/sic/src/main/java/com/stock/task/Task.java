c9434c8bcc879691912645af39904b61a48627f5
BaiJiezi <shaogc@968309.com>
2016-02-02 18:05:51 +0800

    c

--- icc/sic/src/main/java/com/stock/task/Task.java
+++ icc/sic/src/main/java/com/stock/task/Task.java
@@ -1,5 +1,17 @@
 package com.stock.task;
 
+import org.htmlparser.Node;
+import org.htmlparser.NodeFilter;
+import org.htmlparser.Parser;
+import org.htmlparser.filters.NodeClassFilter;
+import org.htmlparser.filters.TagNameFilter;
+import org.htmlparser.tags.Div;
+import org.htmlparser.tags.ImageTag;
+import org.htmlparser.util.NodeIterator;
+import org.htmlparser.util.NodeList;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Date;
 
 /**
@@ -13,11 +25,66 @@ public class Task {
 
     public void execute(){
         System.out.println(new Date() + "  Task  execute");
+
+
+
+
+
+
+
+
     }
 
 
     public String Hi(){
         return "hi";
+    }
+
+
+    public static void main(String[] args){
+        try{
+            Parser parser = new Parser( (HttpURLConnection) (new URL("http://localhost:8080/")).openConnection() );
+
+            NodeFilter filter = new TagNameFilter("div");
+            NodeList list=parser.extractAllNodesThatMatch(filter);
+            for(NodeIterator i = list.elements(); i.hasMoreNodes(); ){
+                Node node = i.nextNode();
+                System.out.println(node.getText());
+                System.out.println(node.toPlainTextString());
+                System.out.println(node.toHtml());
+                System.out.println(node.toHtml(true));
+                System.out.println(node.toHtml(false));
+                System.out.println(node.toString());
+                System.out.println("=================================================");
+
+            }
+
+
+//            for (NodeIterator i = parser.elements (); i.hasMoreNodes(); ) {
+//                Node node = i.nextNode();
+//                System.out.println(node.getText());
+//                System.out.println(node.toPlainTextString());
+//                System.out.println(node.toHtml());
+//                System.out.println(node.toHtml(true));
+//                System.out.println(node.toHtml(false));
+//                System.out.println(node.toString());
+//                System.out.println("=================================================");
+//            }
+
+
+
+
+
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+
+
+
     }
 
 }
--- icc/sic/src/main/webapp/index.jsp
+++ icc/sic/src/main/webapp/index.jsp
@@ -1,5 +1,14 @@
 <html>
-<body>
-<h2>Hello World!22</h2>
-</body>
+    <body>
+        <h2>Hello World!22</h2>
+        <div class="cc">
+            <h3>1111</h3>
+            <h3>2222</h3>
+        </div>
+        <div class="dd">
+            <h3>3333</h3>
+            <h3>4444</h3>
+        </div>
+
+    </body>
 </html>
