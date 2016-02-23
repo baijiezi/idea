03d408d8465ed3de521d902599b7eb98926378f1
BaiJiezi <shaogc@968309.com>
2016-02-23 12:01:38 +0800

    c

---
+++ icc/health-his-qfs/src/main/java/com/stock/task/Task2.java
@@ -0,0 +1,22 @@
+package com.stock.task;
+
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: BaiJiezi
+ * Date: 16-2-2
+ * Time: 下午2:51
+ * To change this template use File | Settings | File Templates.
+ */
+public class Task2 {
+
+    public void execute(){
+        System.out.println(new Date() + "  Task2  execute");
+    }
+
+    public String hi(){
+        return "task2";
+    }
+
+}
