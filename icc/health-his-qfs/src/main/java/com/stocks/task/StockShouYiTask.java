e82d1c1ee000cff6be17c0fcd9e4aa70c4a626d8
BaiJiezi <shaogc@968309.com>
2016-03-18 14:56:00 +0800

    c

---
+++ icc/health-his-qfs/src/main/java/com/stocks/task/StockShouYiTask.java
@@ -0,0 +1,17 @@
+package com.stocks.task;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: BaiJiezi
+ * Date: 16-3-18
+ * Time: 下午2:43
+ * To change this template use File | Settings | File Templates.
+ */
+public class StockShouYiTask {
+
+
+
+
+
+
+}
