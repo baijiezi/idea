e1330bc2a3b1b0aa0e6db377ccbda0bf6a162d11
BaiJiezi <shaogc@968309.com>
2016-03-31 14:01:55 +0800

    c

--- icc/health-his-qfs/src/test/java/com/test/UpdateHangYe.java
+++ icc/health-his-qfs/src/test/java/com/test/UpdateHangYe.java
@@ -19,18 +19,22 @@ public class UpdateHangYe {
         Session session = HibernateUtil.getOpenSession();
         session.beginTransaction();
         StocksDao dao = new StocksDao();
-        String hangYe = "e";
-        String codes = "2010010100220100320100";
+        String hangYe = "f";
+        String codes = "";
         int n = codes.length()/6;
+        System.out.println("length：" + codes.length());
         System.out.println("需要更新行数：" + n);
         for(int i=0; i<n; i++){
-            String code = codes.substring(i*6, (i+1)*6);
-            System.out.println(code);
-            StocksEntity entity = dao.getByCode(code);
-            entity.setHangYe( (entity.getHangYe()==null || entity.equals("")) ? hangYe : entity.getHangYe()+","+hangYe);
-            dao.update(entity, session);
-
-
+            try{
+                String code = codes.substring(i*6, (i+1)*6);
+                System.out.println(code);
+                StocksEntity entity = dao.getByCode(code);
+                entity.setHangYe( (entity.getHangYe()==null || entity.equals("")) ? hangYe : entity.getHangYe()+","+hangYe);
+                dao.update(entity, session);
+            }catch (Exception e){
+                System.out.println("更新异常：");
+                e.printStackTrace();
+            }
         }
         session.getTransaction().commit();
         session.close();
