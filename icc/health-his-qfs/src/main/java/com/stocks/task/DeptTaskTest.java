4af4eaefbb8c1c8afc0286707fc6c238a2f32367
BaiJiezi <shaogc@968309.com>
2016-05-12 18:09:46 +0800

    c

---
+++ icc/health-his-qfs/src/main/java/com/stocks/task/DeptTaskTest.java
@@ -0,0 +1,72 @@
+package com.stocks.task;
+
+import com.message.dao.MessageDao;
+import com.message.entity.MessageEntity;
+
+import java.lang.reflect.Method;
+import java.util.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: BaiJiezi
+ * Date: 16-5-12
+ * Time: 下午5:32
+ * To change this template use File | Settings | File Templates.
+ */
+public class DeptTaskTest {
+    public static void main(String[] args){
+
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("com.message.entity.MessageEntity", "com.message.dao.MessageDao");
+        for(String entityName : map.keySet()){
+            try{
+                String daoName = map.get(entityName);
+                MessageDao dao = (MessageDao)Class.forName(daoName).newInstance();
+                List<MessageEntity> list = dao.getByStatus(0);
+                for(MessageEntity entity : list){
+                    Method[] methods = entity.getClass().getMethods();
+                    StringBuffer result = new StringBuffer(entity.getClass().getName());
+                    result.append("[");
+                    int i = 0;
+                    for(Method method : methods)
+                    {
+                        try
+                        {
+                            String methodName = method.getName();
+                            if(methodName.startsWith("get") && !methodName.equals("getClass"))
+                            {
+                                String fieldName = methodName.substring(3, 4).toUpperCase() + methodName.substring(4, methodName.length());
+                                String value = method.invoke(entity, null).toString();
+                                if(i > 0)
+                                    result.append(", ");
+                                result.append(fieldName).append("=").append(value);
+                                i++;
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                    result.append("]");
+                    System.out.println(result);
+
+                }
+
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+
+
+
+
+
+        }
+
+
+
+
+    }
+
+
+}
