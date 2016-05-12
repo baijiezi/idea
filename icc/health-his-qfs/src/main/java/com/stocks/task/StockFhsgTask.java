b9d2207deefd72d1ad22c47690819042f4a962f9
BaiJiezi <shaogc@968309.com>
2016-05-12 10:06:26 +0800

    c

--- icc/health-his-qfs/src/main/java/com/stocks/task/StockFhsgTask.java
+++ icc/health-his-qfs/src/main/java/com/stocks/task/StockFhsgTask.java
@@ -4,9 +4,7 @@ import com.ning.http.client.AsyncHttpClient;
 import com.ning.http.client.AsyncHttpClientConfig;
 import com.stocks.dao.StocksDao;
 import com.stocks.dao.StocksFhsgDao;
-import com.stocks.dto.StocksDto;
 import com.stocks.dto.StocksFhsgDto;
-import com.stocks.dto.StocksPriceDto;
 import com.stocks.entity.StocksEntity;
 import com.stocks.entity.StocksFhsgEntity;
 import com.stocks.utils.DateUtils;
@@ -16,15 +14,12 @@ import org.hibernate.Session;
 import org.htmlparser.Node;
 import org.htmlparser.NodeFilter;
 import org.htmlparser.Parser;
-import org.htmlparser.filters.CssSelectorNodeFilter;
 import org.htmlparser.filters.TagNameFilter;
-import org.htmlparser.tags.LinkTag;
 import org.htmlparser.tags.TableTag;
 import org.htmlparser.util.NodeIterator;
 import org.htmlparser.util.NodeList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import com.ning.http.client.Response;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -65,76 +60,74 @@ public class StockFhsgTask {
 
             for(StocksEntity stock : list){
                 logger.info("==============================StockFhsgTask:" + stock.getName() + "    " + stock.getCode() + "===============================");
-                if(stock.getId()>=0 && stock.getId()<3800){
-                    try{
+                try{
 //                        if(!stock.getCode().equals("300506")){
 //                            continue;
 //                        }
-                        String url = "http://stock.quote.stockstar.com/dividend/bonus_"+stock.getCode()+".shtml";
-                        Parser parser = new Parser( (HttpURLConnection) (new URL(url)).openConnection() );
-                        parser.setEncoding("GB2312");
-                        TagNameFilter filter = new TagNameFilter("table");
-                        NodeList li = parser.extractAllNodesThatMatch(filter);
-                        System.out.println("list.size: " + li.size());
-                        for(NodeIterator i = li.elements(); i.hasMoreNodes(); ){
-                            TableTag node = (TableTag) i.nextNode();
-                            if(node.getAttribute("class")==null || !node.getAttribute("class").equals("globalTable trHover")){
-                                continue;
-                            }
-                            System.out.println(node.toHtml());
-                            Parser parser1 = new Parser(node.toHtml());
-                            NodeFilter filter2 = new TagNameFilter("tr");
-                            NodeList list2 = parser1.extractAllNodesThatMatch(filter2);
-                            System.out.println("list2.size: " + list2.size());
-                            for(NodeIterator k = list2.elements(); k.hasMoreNodes(); ){
-                                try{
-                                    Node n = k.nextNode();
-                                    Parser parser3 = new Parser(n.toHtml());
-                                    NodeFilter filter3 = new TagNameFilter("td");
-                                    NodeList list3 = parser3.extractAllNodesThatMatch(filter3);
-                                    StocksFhsgDto dto = new StocksFhsgDto();
-                                    dto.setCode(stock.getCode());
-                                    dto.setName(stock.getName());
-                                    dto.setGongGaoRi(DateUtils.strToDate(list3.elementAt(1).toPlainTextString()));
-                                    if(dto.getGongGaoRi().before(dt) || dao.isExit(dto)){
-                                        continue;
-                                    }
-                                    dto.setFenHong(NumberUtils.toIntMilli(list3.elementAt(2).toPlainTextString()));
-                                    dto.setSongGu(NumberUtils.toInt(list3.elementAt(3).toPlainTextString()));
-                                    dto.setZhuanZeng(NumberUtils.toInt(list3.elementAt(4).toPlainTextString()));
-                                    dto.setDengJiRi(DateUtils.strToDate(list3.elementAt(5).toPlainTextString()));
-                                    dto.setChuQuanRi(DateUtils.strToDate(list3.elementAt(6).toPlainTextString()));
-                                    dto.setRemark(list3.elementAt(7).toPlainTextString());
-                                    dto.setMeiGuFenHong(dto.getFenHong()/10);
-                                    if(dto.getMeiGuFenHong()!=null && dto.getMeiGuFenHong()>0){
-                                        StringBuffer sb = new StringBuffer("http://qt.gtimg.cn/r=0.9694567599799484q=");
-                                        if(stock.getExchange().equals("sh") || stock.getExchange().startsWith("沪")){
-                                            sb.append("s_sh").append(stock.getCode());
-                                        }
-                                        if(stock.getExchange().equals("sz") || stock.getExchange().startsWith("深")){
-                                            sb.append("s_sz").append(stock.getCode());
-                                        }
-
-                                        logger.info("url:"+ sb.toString());
-                                        Future r = asyncHttpClient.prepareGet(sb.toString()).execute();
-                                        Response response = (Response) r.get();
-                                        String rs = response.getResponseBody();
-                                        logger.info(rs);
-                                        String str = rs.substring(rs.indexOf("\"")+1, rs.lastIndexOf("\""));
-                                        String[] temp = str.split("~");
-
-                                        dto.setDangQianGuJia(NumberUtils.toIntMilli(temp[3]));
-                                        dto.setShouYiLv(NumberUtils.getBiLv(dto.getMeiGuFenHong(), dto.getDangQianGuJia()));
-                                    }
-                                    data.add(dto);
-                                }catch (Exception e){
-                                    logger.info("StockFhsgTask异常:", e);
+                    String url = "http://stock.quote.stockstar.com/dividend/bonus_"+stock.getCode()+".shtml";
+                    Parser parser = new Parser( (HttpURLConnection) (new URL(url)).openConnection() );
+                    parser.setEncoding("GB2312");
+                    TagNameFilter filter = new TagNameFilter("table");
+                    NodeList li = parser.extractAllNodesThatMatch(filter);
+                    System.out.println("list.size: " + li.size());
+                    for(NodeIterator i = li.elements(); i.hasMoreNodes(); ){
+                        TableTag node = (TableTag) i.nextNode();
+                        if(node.getAttribute("class")==null || !node.getAttribute("class").equals("globalTable trHover")){
+                            continue;
+                        }
+                        System.out.println(node.toHtml());
+                        Parser parser1 = new Parser(node.toHtml());
+                        NodeFilter filter2 = new TagNameFilter("tr");
+                        NodeList list2 = parser1.extractAllNodesThatMatch(filter2);
+                        System.out.println("list2.size: " + list2.size());
+                        for(NodeIterator k = list2.elements(); k.hasMoreNodes(); ){
+                            try{
+                                Node n = k.nextNode();
+                                Parser parser3 = new Parser(n.toHtml());
+                                NodeFilter filter3 = new TagNameFilter("td");
+                                NodeList list3 = parser3.extractAllNodesThatMatch(filter3);
+                                StocksFhsgDto dto = new StocksFhsgDto();
+                                dto.setCode(stock.getCode());
+                                dto.setName(stock.getName());
+                                dto.setGongGaoRi(DateUtils.strToDate(list3.elementAt(1).toPlainTextString()));
+                                if(dto.getGongGaoRi()==null || dto.getGongGaoRi().before(dt) || dao.isExit(dto)){
+                                    continue;
                                 }
+                                dto.setFenHong(NumberUtils.toIntMilli(list3.elementAt(2).toPlainTextString()));
+                                dto.setSongGu(NumberUtils.toInt(list3.elementAt(3).toPlainTextString()));
+                                dto.setZhuanZeng(NumberUtils.toInt(list3.elementAt(4).toPlainTextString()));
+                                dto.setDengJiRi(DateUtils.strToDate(list3.elementAt(5).toPlainTextString()));
+                                dto.setChuQuanRi(DateUtils.strToDate(list3.elementAt(6).toPlainTextString()));
+                                dto.setRemark(list3.elementAt(7).toPlainTextString());
+                                dto.setMeiGuFenHong(dto.getFenHong()/10);
+                                if(dto.getMeiGuFenHong()!=null && dto.getMeiGuFenHong()>0){
+                                    StringBuffer sb = new StringBuffer("http://qt.gtimg.cn/r=0.9694567599799484q=");
+                                    if(stock.getExchange().equals("sh") || stock.getExchange().startsWith("沪")){
+                                        sb.append("s_sh").append(stock.getCode());
+                                    }
+                                    if(stock.getExchange().equals("sz") || stock.getExchange().startsWith("深")){
+                                        sb.append("s_sz").append(stock.getCode());
+                                    }
+
+                                    logger.info("url:"+ sb.toString());
+                                    Future r = asyncHttpClient.prepareGet(sb.toString()).execute();
+                                    Response response = (Response) r.get();
+                                    String rs = response.getResponseBody();
+                                    logger.info(rs);
+                                    String str = rs.substring(rs.indexOf("\"")+1, rs.lastIndexOf("\""));
+                                    String[] temp = str.split("~");
+
+                                    dto.setDangQianGuJia(NumberUtils.toIntMilli(temp[3]));
+                                    dto.setShouYiLv(NumberUtils.getBiLv(dto.getMeiGuFenHong(), dto.getDangQianGuJia()));
+                                }
+                                data.add(dto);
+                            }catch (Exception e){
+                                logger.info("StockFhsgTask异常:", e);
                             }
                         }
-                    }catch (Exception e){
-                        e.printStackTrace();
                     }
+                }catch (Exception e){
+                    e.printStackTrace();
                 }
             }
             List<StocksFhsgEntity> list1 = updateData(data);
