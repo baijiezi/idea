5484561b6ccce5d49751e477870494efda77d207
BaiJiezi <shaogc@968309.com>
2016-06-06 10:05:56 +0800

    c

--- icc/health-his-qfs/src/main/java/com/stocks/task/choose/ChooseIndex.java
+++ icc/health-his-qfs/src/main/java/com/stocks/task/choose/ChooseIndex.java
@@ -1,5 +1,10 @@
 package com.stocks.task.choose;
 
+import com.stocks.dao.StocksPriceDao;
+import com.stocks.entity.StocksPriceEntity;
+
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
  * User: BaiJiezi
@@ -11,7 +16,26 @@ public class ChooseIndex {
 
     public static void main(String[] args){
         ChooseIndex chooseIndex = new ChooseIndex();
+        chooseIndex.niZhang();
+
+    }
+
+    public String niZhang(){
+        StocksPriceDao dao = new StocksPriceDao();
+        List<StocksPriceEntity> list = dao.getByCode("000001_2");
+        for(StocksPriceEntity entity : list){
+            if(entity.getZhangFu()!=null && entity.getZhangFu()<-2000){
+                List<StocksPriceEntity> data = dao.getByDate(entity.getDate());
+                for(StocksPriceEntity priceEntity : data){
+                    if(priceEntity.getZhangFu()>=5000 && priceEntity.getCode().startsWith("600")){
+                        System.out.println(priceEntity.getCode() + "  " + priceEntity.getDate() + "  " + priceEntity.getPriceTrends());
+                    }
+                }
+            }
+        }
+        return null;
 
 
     }
+
 }
