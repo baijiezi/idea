97608b9db8de410a81b6959f95ce869deda89efa
BaiJiezi <shaogc@968309.com>
2016-06-12 16:08:23 +0800

    c

--- icc/health-his-qfs/src/main/java/com/stocks/task/choose/ChooseIndex.java
+++ icc/health-his-qfs/src/main/java/com/stocks/task/choose/ChooseIndex.java
@@ -2,8 +2,12 @@ package com.stocks.task.choose;
 
 import com.stocks.dao.StocksPriceDao;
 import com.stocks.entity.StocksPriceEntity;
+import com.stocks.utils.DateUtils;
+import com.stocks.utils.HibernateUtil;
+import com.stocks.utils.NumberUtils;
+import org.hibernate.Session;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,7 +20,7 @@ public class ChooseIndex {
 
     public static void main(String[] args){
         ChooseIndex chooseIndex = new ChooseIndex();
-        chooseIndex.niZhang();
+        chooseIndex.jiaGeYuZengZhangLv();
 
     }
 
@@ -34,8 +38,64 @@ public class ChooseIndex {
             }
         }
         return null;
+    }
 
 
+//    2-3 add:2 desc:4
+//    3-4 add:8 desc:15
+//    4 add:17 desc:35
+//    5 add:20 desc:43
+//    6 add:49 desc:49
+//    7 add:52 desc:37
+//    8 add:38 desc:39
+//    9 add:49 desc:36
+//    10 add:61 desc:33
+//    11 add:47 desc:41
+//    12 add:42 desc:37
+//    13 add:44 desc:30
+//    14 add:29 desc:24
+//    15 add:42 desc:20
+//    16 add:21 desc:16
+//    17 add:22 desc:16
+//    18 add:20 desc:18
+//    19 add:21 desc:7
+//    20 add:10 desc:5
+//    21 add:12 desc:9
+//    22 add:13 desc:12
+//    23 add:10 desc:11
+//    24 add:13 desc:9
+//    25 add:8 desc:4
+//    26 add:10 desc:10
+//    27 add:14 desc:7
+    public void jiaGeYuZengZhangLv(){
+        Date date1 = DateUtils.strToDate("2016-03-10");
+        Date date2 = DateUtils.strToDate("2016-05-19");
+        StocksPriceDao dao = new StocksPriceDao();
+        List<StocksPriceEntity> list = dao.getByDate(date1);
+        Session session = HibernateUtil.getOpenSession();
+        session.beginTransaction();
+        int add=0, desc=0;
+        for(StocksPriceEntity entity : list){
+            if(entity.getShouPan()>0){
+                StocksPriceEntity entity2 = dao.getByDateAndCode(date2, entity.getCode(), session);
+                if(entity2!=null && entity2.getShouPan()>0){
+                    int biLv = NumberUtils.getBiLv(entity2.getShouPan() - entity.getShouPan(), entity.getShouPan());
+                    if(biLv > 0){
+                        add++;
+                    }
+                    if(biLv < 0){
+                        desc++;
+                    }
+
+                }
+            }
+        }
+        session.getTransaction().commit();
+        session.close();
+        HibernateUtil.closeSessionFactory();
+
+        System.out.println("add:" + add);
+        System.out.println("desc:" + desc);
     }
 
 }
