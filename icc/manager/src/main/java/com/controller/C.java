68d4dd66e2fbfe50fdfa040746f2a73e0a27b7f5
BaiJiezi <shaogc@968309.com>
2016-09-01 18:03:22 +0800

    c

---
+++ icc/manager/src/main/java/com/controller/C.java
@@ -0,0 +1,11 @@
+package com.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: BaiJiezi
+ * Date: 16-9-1
+ * Time: 下午6:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class C {
+}
