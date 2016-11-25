13a0d80479cae92cada2943be00b6098209948bb
BaiJiezi <shaogc@968309.com>
2016-11-25 09:57:46 +0800

    c

--- icc/health-his-qfs/src/main/java/com/stocks/dao/StocksPriceDao.java
+++ icc/health-his-qfs/src/main/java/com/stocks/dao/StocksPriceDao.java
@@ -187,7 +187,7 @@ public class StocksPriceDao implements IBaseDao{
         List<StocksPriceEntity> list = new ArrayList<StocksPriceEntity>();
         try{
             String endDate = DateUtils.getSimpleDate(date);
-            date = DateUtils.addDate(date, -100);
+            date = DateUtils.addDate(date, -150);
             String startDate = DateUtils.getSimpleDate(date);
             Query query = session.createQuery(" from StocksPriceEntity s where s.code = '" + code + "' and s.date >= '" + startDate + "' and s.date < '" + endDate + "'");
             list = query.list();
