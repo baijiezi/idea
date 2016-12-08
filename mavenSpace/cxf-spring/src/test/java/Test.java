d37d0b11a9b1830b4e31431aa964c70ed4098450
BaiJiezi <shaogc@968309.com>
2016-12-08 17:14:01 +0800

    c

---
+++ mavenSpace/cxf-spring/src/test/java/Test.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: BaiJiezi
+ * Date: 16-12-8
+ * Time: 下午5:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
